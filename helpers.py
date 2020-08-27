2f57f31e05eb795986a3a82970a2c79f235b481b
Qi Qi <qqi@mit.edu>
2020-08-27 09:11:34 -0700

    Switch to greek alphabet for symbols

--- helpers.py
+++ helpers.py
@@ -5,6 +5,11 @@ enclosed_alp = [
     '\u24AE', '\u24AF', '\u24B0', '\u24B1', '\u24B2', '\u24B3',
     '\u24B4', '\u24B5'
 ]
+greek_alp = [
+    '\u03b1', '\u03b2', '\u03b3', '\u03b4', '\u03b5', '\u03b6',
+    '\u03b7', '\u03b8', '\u03b9', '\u03ba', '\u03bb', '\u03bc',
+    '\u03bd', '\u03be', '\u03bf', '\u03c0', '\u03c1', '\u03c3',
+    '\u03c4', '\u03c5', '\u03c6', '\u03c7', '\u03c8', '\u03c9']
 
 
 def color(string, color):
@@ -25,16 +30,17 @@ def color(string, color):
 class SymbolAssigner(object):
     def __init__(self):
         self.symbol_index = 0
+        self.alp = greek_alp
 
     def assign(self):
-        to_return = enclosed_alp[self.symbol_index]
+        to_return = self.alp[self.symbol_index]
         self.symbol_index += 1
-        if self.symbol_index >= len(enclosed_alp):
+        if self.symbol_index >= len(self.alp):
             self.symbol_index = 0
         return to_return
 
     def cur_symbol(self):
-        return enclosed_alp[self.symbol_index]
+        return self.alp[self.symbol_index]
 
     def reset(self):
         self.symbol_index = 0
